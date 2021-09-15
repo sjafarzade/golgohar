fc50cea83c37943f376aa94f55c21ac5ba69fc89
Soheil Jafarzade <soheil.jafarzade@gmail.com>
2021-09-15 17:51:07 +0430

    change DataGrid to MaterialTable

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,20 +1,41 @@
 import * as React from "react";
-import {
-  DataGrid,
-  GridColDef,
-  GridValueGetterParams,
-} from "@material-ui/data-grid";
+import MaterialTable, { MTableToolbar } from "material-table";
+import { tableIcons } from "../const.jsx";
 
 export default function DataTable(props) {
   const { columns = [], rows = [] } = props;
   return (
     <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={rows}
+      <MaterialTable
+        icons={tableIcons}
+        title={""}
+        data={rows}
         columns={columns}
         pageSize={5}
-        checkboxSelection
-        disableSelectionOnClick
+        cellEditable={{
+          onCellEditApproved: (newValue, oldValue, rowData, columnDef) => {
+            return new Promise((resolve, reject) => {
+              setTimeout(resolve, 1000);
+            });
+          },
+        }}
+        options={{
+          rowStyle: {
+            fontWeight: "500",
+            lineHeight: "1.5rem",
+            fontSize: "0.875rem",
+            fontFamily: '"Roboto", "Helvetica", "Arial", "sans-serif"',
+            letterSpacing: "0.01071em",
+            // width: "auto",
+            // textAlign: "center",
+          },
+          headerStyle: {
+            // width: "auto",
+            // textAlign: "center",
+          },
+        }}
+        // checkboxSelection
+        // disableSelectionOnClick
       />
     </div>
   );
