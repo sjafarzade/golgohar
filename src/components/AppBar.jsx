b964c3c8bebfac530bcc9da99df86392760bef1c
Soheil Jafarzade <soheil.jafarzade@gmail.com>
2021-09-16 10:32:16 +0430

    change deprecated

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import "@progress/kendo-theme-material/dist/all.css";
 // import "../styles/App.scss";
 import clsx from "clsx";
-import { fade, makeStyles, useTheme } from "@material-ui/core/styles";
+import { alpha, makeStyles, useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -114,9 +114,9 @@ const useStyles = makeStyles((theme) => ({
   search: {
     position: "relative",
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
     "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
